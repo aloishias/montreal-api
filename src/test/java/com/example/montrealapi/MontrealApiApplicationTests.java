a2575825cfaee8c73aa751e5a84dcbaeae3f6b6d
Aloïs HIAS <aloishias@MacBook-Pro-de-Alois.local>
2021-07-20 11:25:15 +0200

    Init spring boot project

---
+++ src/test/java/com/example/montrealapi/MontrealApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.montrealapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontrealApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
