fc69710276dd5be31015a603f02768435b8929e5
Aloïs HIAS <aloishias@MacBook-Pro-de-Alois.local>
2022-01-04 11:15:40 +0100

    Add swagger documentation

---
+++ src/main/java/com/example/montrealapi/swagger/SpringFoxConfig.java
@@ -0,0 +1,26 @@
+package com.example.montrealapi.swagger;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    private String hostUrl = "localhost:9090";
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .host(hostUrl)
+                .groupName("Spring Actuator")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
